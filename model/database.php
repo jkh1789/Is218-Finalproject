bb63943b5cf788d9fa3dfff1cbb061f8c9a764c9
jkh1789 <kj59@njit.edu>
2017-04-20 15:30:31 -0400

    modified database.php

---
+++ model/database.php
@@ -0,0 +1,14 @@
+<?php
+    $dsn = 'mysql:host=sql2.njit.edu;dbname=kj59';
+    $username = 'kj59';
+    $password = 'zYL3aNQK';
+
+    try {
+    	$db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+	include('database_error.php');
+	exit();
+
+    }
+?>
