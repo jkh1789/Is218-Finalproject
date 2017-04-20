86826e54dde5531ca5491f7125641f84608c11d2
jkh1789 <kj59@njit.edu>
2017-04-20 15:35:59 -0400

    add database_error.php

---
+++ model/database_error.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>To Do List</title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+    </head>
+
+<!-- the body section -->
+<body>
+     <header><h1>My Guitar Shop</h1></header>
+
+     <main>
+          <h1>Database Error</h1>
+	  <p>There was an error connecting to the database.</p>
+	  <p>Error message: <?php echo $error_message; ?></p>
+	  <p>&nbsp;</p>
+     </main>
+
+     <footer>
