280de0c6a9a3d02dffee0b6aafc5d93f80a43b2b
jkh1789 <kj59@njit.edu>
2017-04-20 14:08:30 -0400

    add login page

---
+++ Login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+
+  <head>
+   <meta charset = utf-8>
+  </head>
+
+<body>
+
+<!--login and password-->
+<center>
+<div style="position:relative;top:150px;background-image:url(images/Login.png);width:400px;height:250px;"> 
+   <div style=""position:absolute;top:154px;left:54px;"><input type="text" name="uid" style="width:187px; height:17px;></div>
+   <div style="position:absolute;top:182px;left:54px;"><input type="password" name="upwd" style="width:187px;height17px;"></div>
+   <div style="position:absolute;top:152px;left:240px;width:115px;height:62px;"><a href="#"><img src="images/bt_login.jpg" border=0 id="login"></div>
+</div>
+
+</body>
+
+</html>
+
---
+++ view/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <p class="copyright">
+    	$copy; <?php echo date("Y/m/d"); ?> KeeHyun Jung
+    </p>
+</footer>
+</body>
+</html>
+
