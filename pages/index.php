41b54647a9ac1113020df527c14bd6af243ae744
jkh1789 <kj59@njit.edu>
2017-04-24 16:32:31 -0400

    modified index.php

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,24 @@
+<?php 
+include ("../model/database.php");
+
+$itemsQuery = $db->prepare("
+	SELECT id, message, submissiondate, duedate, Iscomplete
+	FROM items
+	WHERE user = :user
+");
+
+$itemsQuery->execute([
+	'user' => $_SESSION['username']
+]);
+
+$items = $itemsQuery->rowCount() ? $itemsQuery : [];
+
+foreach($itmes as $items) {
+	print_r($item);
+
+
+?>
+
 
 
 <!DOCTYPE html>
---
+++ pages/logout.php
@@ -0,0 +1,6 @@
+<?php include ("session.php"); ?>
+<?php
+session_destroy;
+ echo "<meta https-equiv='refresh' content = '0;
+ url=https://web.njit.edu/~kj59/is218_FinalProject/pages/login.php'>";
+?>
