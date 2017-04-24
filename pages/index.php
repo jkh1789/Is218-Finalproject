caa995feda611651f63be3e6a68128d783cc5baf
jkh1789 <kj59@njit.edu>
2017-04-23 22:20:53 -0400

    modified index.php

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,4 @@
-<?php include ("session.php"); ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -50,7 +50,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
+                <a class="navbar-brand" href="index.php">SB Admin v2.0</a>
             </div>
             <!-- /.navbar-header -->
 
@@ -60,17 +60,6 @@
                         <i class="fa fa-envelope fa-fw"></i> <i class="fa fa-caret-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-messages">
-                        <li>
-                            <a href="#">
-                                <div>
-                                    <strong>John Smith</strong>
-                                    <span class="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-                                </div>
-                                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-                            </a>
-                        </li>
                         <li class="divider"></li>
                         <li>
                             <a href="#">
