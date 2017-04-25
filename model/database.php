799406222fbb7154c2b20449377fd47bc7d8f442
jkh1789 <kj59@njit.edu>
2017-04-25 03:04:09 -0400

    modified css

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
     $dsn = 'mysql:host=sql2.njit.edu;dbname=kj59';
     $username = 'kj59';
-    $password = 'zYL3aNQK';
+    $password = 'zYL3aNQk';
 
     try {
     	$db = new PDO($dsn, $username, $password);
@@ -9,6 +9,5 @@
         $error_message = $e->getMessage();
 	include('database_error.php');
 	exit();
-
     }
 ?>
--- pages/index.php
+++ pages/index.php
@@ -6,8 +6,8 @@
 	<meta charset="URF-8">
 	<title>To do</title>
 
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
 	<link rel="stylesheet" href="css/main.css">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,17 +16,23 @@
     <body>
 	><div class="list">
 	    <h1 class="header">To do.</h1>
-
-	    <ul>
-	    	<li><span class="item">Pick up shopping</span></li>
-		<li><span class="item done">Learn php</span></li>
+	   
+	   <ul class="items">
+	    	<li>
+		    <span class="item">Pick up shopping</span>
+		    <a href="#" class="done-botton">Mark as done</a>
+	       </li>
+		<li>
+		    <span class="item done">Learn php</span></li>
 	    <ul>
 
 	    <form class="item-add"  action="add.php" method="post">
 		<input type="text" name="name" placeholder="Type a new item here." class="input"
 		autocomplete="off" required>
 		<input type="submit" value="Add" Class="">
-  
+	    </form>
+
+	 </div>
  
 
 </body>
